0bfc71fa925d3a4cbe46258a63d2c270d2f93574
John Lathouwers <john.lathouwers@oracle.com>
2022-06-20 11:16:16 +0000

    runAlways

--- liquibase/data/inventory_table.sql
+++ liquibase/data/inventory_table.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql			
--- changeset insert_static:1
+-- changeset insert_static:1 runAlways:true failOnError:true
 TRUNCATE TABLE INVENTORY;                
 INSERT into INVENTORY values ('sushi', '1468 WEBSTER ST,San Francisco,CA', 0);
 INSERT into INVENTORY values ('pizza', '1469 WEBSTER ST,San Francisco,CA', 0);
